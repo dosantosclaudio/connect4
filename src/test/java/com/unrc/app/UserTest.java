3fe04e128c3a007482f9105399ba5651f6e2b42e
Claudio Dosantos <cdosantosclaudio@gmail.com>
2015-05-21 14:11:35 -0300

    21/05 Rank incluido

--- src/test/java/com/unrc/app/UserTest.java
+++ src/test/java/com/unrc/app/UserTest.java
@@ -26,13 +26,68 @@ public class UserTest {
         Base.close();
     }
 
-
     @Test
     public void shouldValidateMandatoryFields(){
       User user = new User();
-
-      user.set("email", "juan@hotmail.coms");
-
+      user.set("last_name","Gerrard");
+      the(user).shouldNotBe("valid");
+      user.set("email", "juan@hotmail.com");
+      the(user).shouldNotBe("valid");
+      user.set("password","123456789");
       the(user).shouldBe("valid");
+    }
+
+
+    @Test
+    public void shouldValidateSignUp(){
+        try{
+            User.insert("juan@hotmail.es","Juan","Perez","12345");      //The password is invalid, it is too shortest, so it mustn't sign up to new account.
+        }catch(UserException e){
+                User u=User.findFirst("email = ?", "juan@hotmail.es" );
+                the(u).shouldBeNull(); 
+        }
+        try{
+            User.insert("juan@hotmail.es","Juan","Perez","12345678");
+            User.insert("juan@hotmail.es","Juan","Lopez","123456789");
+        }catch(UserException e){
+            User u=User.findFirst("email = ?", "juan@hotmail.es" );     //The second insert mustn't work because "juan@hotmail.es" allready registered.
+            assertEquals(u.get("password"),"12345678");
+        }
+    }
+
+    @Test
+    public void shouldValidateSignIn(){
+        try{
+        User.insert("jg@gmail.com","Juan","Giroud","arsenalfc");
+        }catch(UserException e){
+
+        }
+        User u=null;
+        try{
+            u=User.signIn("jg2@gmail.com","dddddddddddd");
+            the(u).shouldBeNull();
+        }catch(UserException e){
+            the(u).shouldBeNull();          //The account doesn't exist
+        }
+        try{
+            u=User.signIn("jg@gmail.com","arsenalf");
+            the(u).shouldBeNull();
+        }catch(UserException e){
+            the(u).shouldBeNull();          //Invalid password
+        }
+        try{
+            u=User.signIn("jg2@gmail.com","arsenalfc");
+            the(u).shouldBeNull();
+        }catch(UserException e){
+            the(u).shouldBeNull();          //Invalid email adress
+        }
+        try{
+            u=User.signIn("jg@gmail.com","arsenalfc");
+            the(u).shouldNotBeNull();       //The email and password are correct.
+        }catch(UserException e){
+            the(u).shouldNotBeNull();       
+        }
+       
+
     }
 }
