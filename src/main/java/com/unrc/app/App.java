43f8b6facabded0ac5498c00296d97ecc77546d0
martinlopez <millopez94@gmail.com>
2015-06-05 09:18:44 -0300

    se ha agregado spark a pom y se ha modificado App.java, ahora importa librerias de spark,

--- src/main/java/com/unrc/app/App.java
+++ src/main/java/com/unrc/app/App.java
@@ -1,6 +1,6 @@
 package com.unrc.app;
 import java.util.*;
-
+import static spark.Spark.*;
 import com.unrc.app.User;
 import org.javalite.activejdbc.Base;
 import org.javalite.activejdbc.LazyList;
@@ -10,16 +10,17 @@ import java.io.Console;
 public class App
 {
 	public App(){
-		Menu m=new Menu();
+	//	Menu m=new Menu();
 	}
 
     public static void main( String[] args )
     {
-        Base.open("com.mysql.jdbc.Driver", "jdbc:mysql://localhost/connect4", "root", "root");
+        get("/hello", (req, res) -> "Hello World");
+    /*    Base.open("com.mysql.jdbc.Driver", "jdbc:mysql://localhost/connect4", "root", "root");
 
         App a=new App();
         
-        Base.close();
+        Base.close();*/
     }
 	
 }
